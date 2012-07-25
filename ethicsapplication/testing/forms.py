0e2565ded8d1d0aa860fc1db2b555b382dd31e7a
Jason Marshall <ctrlspc@gmail.com>
2012-07-25 16:14:58 +0100

    forgot to remove the url wiring

--- ethicsapplication/testing/forms.py
+++ ethicsapplication/testing/forms.py
@@ -7,20 +7,36 @@ Created on Jul 25, 2012
 '''
 
 from django.test import TestCase
+from ethicsapplication.forms import EthicsApplicationForm
 
 class FormsTest(TestCase):
     
     def test_EthicsApplication_form_title(self):
         '''
-            Check that the form provdes a field that cannot be blank for the 
+            Check that the form provides a field that cannot be blank for the 
             application title
         '''
+        form = EthicsApplicationForm()
         
-        self.assert_(False, 'Not yet implemented')
+        self.assertTrue('title' in form.fields)
 
     def test_EthicsApplication_form_ignored_fields(self):
         '''
             Check that the form does not display a field for either 
             Principle investigator, nor application.
         '''
-        self.assert_(False, 'Not yet implemented')
+        form = EthicsApplicationForm()
+        
+        self.assertFalse('principle_investigator' in form.fields)
+        self.assertFalse('application_form' in form.fields)
+        
+    def test_EthicsApplication_form_title_must_be_present(self):
+        '''
+            The form must require a non blank string for the title field.
+        '''
+        incorrect_data = {'data': {'title': '',}}
+        form = EthicsApplicationForm(incorrect_data)
+        
+        form.is_valid()
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['title'], [u'This field is required.'])
--- ethicsapplication/views.py
+++ ethicsapplication/views.py
@@ -1,6 +1,9 @@
 # Create your views here.
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from ethicsapplication.forms import EthicsApplicationForm
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
 
 
 #@login_required
@@ -13,4 +16,13 @@ def create_application(request):
         
         All valid users of the system should be able to create ethics applications.
     '''
-    return HttpResponse('create_application')
+    
+    if request.method == 'POST':
+        pass
+    
+    else:
+        
+        form = EthicsApplicationForm()
+        
+    return render_to_response('ethicsapplication/create.html', {'form':form},
+                              context_instance=RequestContext(request))
