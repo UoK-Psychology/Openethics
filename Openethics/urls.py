b7397a19de8e9c8c8e14b8e8c05048377565acbe
Jason Marshall <ctrlspc@gmail.com>
2012-08-29 16:09:44 +0100

    Added the login/out urls conf wiring

--- Openethics/urls.py
+++ Openethics/urls.py
@@ -9,6 +9,9 @@ urlpatterns = patterns('',
       url(r'^admin/', include(admin.site.urls)),#admin site
       (r'^$', direct_to_template, { 'template': 'index.html' }, 'index'), #basic index page
       
+      url(r'^accounts/login/$', view ='django.contrib.auth.views.login', name='auth_login'),
+      url(r'^accounts/logout/$', view ='django.contrib.auth.views.logout', name='auth_logout'),
+      
       # This is for overiding from outside apps   
-      (r'^application/', include('ethicsapplication.urls')),
+      url(r'^application/', include('ethicsapplication.urls')),
 )
