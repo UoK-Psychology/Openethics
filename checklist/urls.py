8eaacbefcf8907c7a14d1cda57d0fda32133c7d4
Jason Marshall <ctrlspc@gmail.com>
2012-09-11 12:54:32 +0100

    configured the urls for the checklist app

--- checklist/urls.py
+++ checklist/urls.py
@@ -2,8 +2,14 @@ from django.conf.urls import patterns, url
 
 
 urlpatterns = patterns('',
-      url(r'^$',
-          view ='', 
-          name='do_checklist'), #do the checklist questionnaire for the first time
+      url(r'^start/(?P<ethics_application_id>\d+)/$',
+          view ='checklist.views.start_checklist', 
+          name='start_checklist'), #start the checklist for a given ethics_application
+      url(r'^do/(?P<questionnaire_id>\d+)/$',
+          view ='questionnaire.views.do_questionnaire', 
+          name='do_checklist',
+          kwargs= {'template_name':'checklist/checklist_form.html',
+                   'next_form_name':'handle_next_questiongroup_form',
+                    'finished_url':'/'}),                  
      
 )
