a5f1b67cbff862e691fecf7cc90185483ce6af3d
john allen <john@allenjohn.co.uk>
2012-09-13 16:39:18 +0100

    configured basic application groups
    write logic that configures these basic application groups into a
    questionnaire

--- checklist/tests/views.py
+++ checklist/tests/views.py
@@ -108,9 +108,96 @@ class StartChecklistTestCase(TestCase):
         self.client.login(username='test', password='password') 
         self.assertRaises(ImproperlyConfigured, self.client.get,reverse('start_checklist', kwargs={'ethics_application_id':self.ethicsApplication.id}))
 
+                
         
+class FinishedChecklistTestCase(TestCase):
+
+    def setUp(self):
         
+        self.user = User.objects.create_user('test', 'test@test.com', 'password')
+        self.ethicsApplication = EthicsApplication.objects.create(title='test application', principle_investigator=self.user)
+        self.url = reverse('finished_checklist', kwargs={'ethics_application_id':self.ethicsApplication.id})
         
+    def test_get_request_not_logged_in(self):
+        '''
+        If user is not logged in then they should get a page forbidden 403
+        '''
+        response = self.client.get(self.url)
+        self.assertEquals (403, response.status_code )
         
+    def test_get_invalid_application_id(self):
+        '''
+        If ethics applicatin ID is invalid we should get a 404 does not exist
+        '''
+        self.client.login(username='test', password='password') 
+        url = reverse('finished_checklist', kwargs={'ethics_application_id':971234})
+        response = self.client.get(url)
+        self.assertEquals (404, response.status_code )
+        
+    def test_get_valid_firsttime(self):
+        '''
+        If ethics application model is valid and doesnt have an existing application 
+        form questinnaire and user is logged in then:
+            - a new application form questionnaire is created and associated to the 
+            ethics application model
+            - the application form questionnaire has the correct question groups
+            configured based on settings.BASIC_APPLICATION_GROUPS
+            - we should be redirected to the view page for the ethics application
+        '''
+        settings.BASIC_APPLICATION_GROUPS = [1,1,1]  # only group 1 is setup in the local fixture
+        self.client.login(username='test', password='password') 
+        response = self.client.get(self.url)
+        
+        self.ethicsApplication = EthicsApplication.objects.get(id=self.ethicsApplication.id)  #refresh our instance from the DB
+        self.assertIsInstance(self.ethicsApplication.application_form, Questionnaire)
+        
+        applicationform_groups = [group.id for group in self.ethicsApplication.application_form.get_ordered_groups()]
+        self.assertEqual(settings.BASIC_APPLICATION_GROUPS, applicationform_groups)    
+        
+        redirect_url = reverse('application_view', kwargs={'application_id':self.ethicsApplication.id})
+        self.assertEquals (302, response.status_code )
+        self.assertEquals (response['Location'], 'http://testserver' + redirect_url)
+        
+    def test_get_valid_notfirsttime(self):
+        '''
+        If ethics application model is valid and does have an existing application 
+        form questinnaire and user is logged in then:
+            - check that the application form questionnaire doesn't change
+            - we should be redirected to the view page for the ethics application
+        '''
+        settings.BASIC_APPLICATION_GROUPS = [1,1,1]
+        self.client.login(username='test', password='password') 
+        test_questionnaire=Questionnaire.objects.create(name='testname')
+        self.ethicsApplication.application_form = test_questionnaire
+        self.ethicsApplication.save() 
+              
+        response = self.client.get(self.url)
+        #check that our EA has not been changed by getting it back from the DB and compairing it with
+        #the one we created and stored away at the beginning
+        self.ethicsApplication = EthicsApplication.objects.get(id=self.ethicsApplication.id)  #refresh our instance from the DB
+        self.assertEqual(self.ethicsApplication.application_form, test_questionnaire)
+        #check we got sent to the expected url
+        redirect_url = reverse('application_view', kwargs={'application_id':self.ethicsApplication.id})
+
+        self.assertEquals (302, response.status_code )
+        self.assertEquals (response['Location'], 'http://testserver' + redirect_url)
+
+        
+    def test_basic_application_group_setting_improperly_configured(self):
+        '''
+            If there is no settings.BASIC_APPLICATION_GROUPS or the groups it references 
+            do not exist then an ImproperlyConfigured
+            exception will be thrown
+        '''
+        if hasattr(settings, 'BASIC_APPLICATION_GROUPS'):
+            del(settings.BASIC_APPLICATION_GROUPS)
+        
+        self.client.login(username='test', password='password') 
+        self.assertRaises(ImproperlyConfigured, self.client.get, self.url)
+        
+        settings.BASIC_APPLICATION_GROUPS = [99999,]
+        
+        self.client.login(username='test', password='password') 
+        self.assertRaises(ImproperlyConfigured, self.client.get, self.url)
         
         
--- checklist/views.py
+++ checklist/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import get_object_or_404
 from ethicsapplication.models import EthicsApplication
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
 
 @login_required
 def start_checklist(request, ethics_application_id):
@@ -38,3 +39,33 @@ def start_checklist(request, ethics_application_id):
     url = '%s?on_success=%s' % (base_url, application_view_url)
     return HttpResponseRedirect(url)
         
+        
+def finished_checklist(request, ethics_application_id):
+    if not request.user.is_authenticated():
+        raise PermissionDenied()
+    
+    ethics_application = get_object_or_404(EthicsApplication, pk=ethics_application_id)
+    
+    if not hasattr(settings, 'BASIC_APPLICATION_GROUPS'):
+            raise ImproperlyConfigured('BASIC_APPLICATION_GROUPS ID not configured')
+    else:
+        application_groups = []
+        for primary_key in settings.BASIC_APPLICATION_GROUPS:
+            try:
+                group = QuestionGroup.objects.get(pk=primary_key)
+                application_groups.append(group)
+            except ObjectDoesNotExist:
+                raise ImproperlyConfigured('Question group does not exist for configured id of: %s' % primary_key)
+    
+    if ethics_application.application_form is None:
+        application_form_questionnaire = Questionnaire.objects.create(name='Application form for application id: %s' % ethics_application.id)
+        for group in application_groups:
+            application_form_questionnaire.add_question_group(group)
+        
+        ethics_application.application_form = application_form_questionnaire
+        ethics_application.save()
+           
+    return HttpResponseRedirect(reverse('application_view', kwargs={'application_id':ethics_application_id}))
+
+
+
