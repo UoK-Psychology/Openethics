a09216a01f65bce58f94890f16b736cb7600daa2
Jason Marshall <ctrlspc@gmail.com>
2012-09-14 19:21:06 +0100

    updated generic script!

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ cd $WORKSPACE
 virtualenv -q ve
 source ./ve/bin/activate
 pip install -E ./ve -r requirements.txt
-cd $WORKSPACE/projectname
+cd $WORKSPACE/Openethics
 python manage.py migrate
 python manage.py jenkins
