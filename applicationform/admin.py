f460567f6cb287a7d75f1d3539ee5426fcf166c0
Jason Marshall <ctrlspc@gmail.com>
2012-09-20 18:44:09 +0100

    added the link model into admin

---
+++ applicationform/admin.py
@@ -0,0 +1,10 @@
+'''
+Created on 27 Jul 2012
+
+@author: jjm20
+'''
+from django.contrib import admin
+from applicationform.models import FullApplicationChecklistLink
+
+
+admin.site.register(FullApplicationChecklistLink)
