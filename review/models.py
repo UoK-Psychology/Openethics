fa7bf09d16cde0815775f5d06b73878afc478e9e
Jason Marshall <ctrlspc@gmail.com>
2012-09-25 19:53:06 +0100

    stubbed the functionality for the committee manager
    and its get_next_reviewer function

--- review/models.py
+++ review/models.py
@@ -1,6 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+
+class CommitteeManager(models.Manager):
+    '''
+        The deafult manager for the Committee model
+    '''
+    
+    def get_next_free_reviewer(self):
+        '''
+            This function should return the committe member who has the smallest
+            application count. If there are two members with the same count then the
+            user with the highest pk is returned. If there are no committee members
+            then this function will return None
+        
+        '''
+        return False
+    
 class Committee(models.Model):
     '''
         This model represents the review committee.
@@ -10,3 +27,5 @@ class Committee(models.Model):
     '''
     member = models.ForeignKey(User)
     count = models.IntegerField(max_length=3, default=0)
+
+    objects = CommitteeManager()
