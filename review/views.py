d89a813aad19d5b352defb05932ade44f5dae979
Jason Marshall <ctrlspc@gmail.com>
2012-09-27 19:01:15 +0100

    implemented the submit_for_review view

--- review/views.py
+++ review/views.py
@@ -3,9 +3,24 @@ from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from permissions.utils import has_permission
 from workflows.utils import do_transition
+from django.shortcuts import get_object_or_404
+from django.core.exceptions import PermissionDenied
+from ethicsapplication.models import EthicsApplication
+
+
+
 def submit_for_review(request, ethics_application_id):
     '''
-    
+        This view will ascertain if the submit_for_review transition can be
+        carried out on the specified ethicsApplication, by the logged in user.
+        If any of the following goes wrong then a forbidden exception will be raised:
+        1.User is not logged in
+        2.User does not have the submit permission for this application
+        3.The transition is not allowed
     '''
+    ethics_application = get_object_or_404(EthicsApplication, pk=ethics_application_id)
+    
+    if not request.user.is_authenticated() or not has_permission(request.user, 'submit') or not do_transition(ethics_application, 'submit_for_review', request.user):
+        raise PermissionDenied()
     
     return HttpResponseRedirect(reverse('index_view'))
