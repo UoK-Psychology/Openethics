5f26f36e5de5e3db4830e2b0368e2c3eae5a8f58
Jason Marshall <ctrlspc@gmail.com>
2012-10-23 16:01:19 +0100

    added authorization and filed exclusion
    to make this resource more secure - although it is still not great as
    currently anyone with an api key can interogate all of the user objects

--- root/api.py
+++ root/api.py
@@ -1,7 +1,12 @@
 from django.contrib.auth.models import User
 from tastypie.resources import ModelResource
+from tastypie.authentication import ApiKeyAuthentication
+from tastypie.constants import ALL
 
 class UserResource(ModelResource):
     class Meta:
         queryset = User.objects.all()
         resource_name = 'user'
+        authentication = ApiKeyAuthentication()
+        filtering = {'email': ALL}
+        excludes = ['email', 'password', 'is_active', 'is_staff', 'is_superuser']
