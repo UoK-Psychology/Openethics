c3fe965f44dc996a172fb6bc143881fa329c9bc6
Jason Marshall <ctrlspc@gmail.com>
2012-11-07 12:52:12 +0000

    implemented the tests

--- review/tests/views.py
+++ review/tests/views.py
@@ -96,43 +96,71 @@ class EvaluateApplicationFormTests(TestCase):
         '''
             The common setup for all tests
         '''
-
+        self.test_user = User.objects.create_user('test', 'teset@test.com', 'password')
+        self.test_application = EthicsApplication.objects.create(title='test', principle_investigator=self.test_user)
+        self.valid_accept_url = reverse('accept_application', kwargs={'ethics_application_id':self.test_application.id})
+        self.valid_reject_url = reverse('reject_application', kwargs={'ethics_application_id':self.test_application.id})
+        self.valid_url_permutations = [self.valid_accept_url, self.valid_reject_url]
     def test_logged_in(self):
         '''
             If the user is not logged in then they should eb redirected to the login
             screen
         '''
+        #accept url
+        for url in self.valid_url_permutations:
+            response = self.client.get(url)
+            self.assertRedirects(response, '/accounts/login/?next=%s' % url )
         
-        self.assertTrue(False)
+        
         
     def test_invalid_parameters(self):
         '''
             If the ethics_application_id is not in the db then this should cause a 
             404 error.
         '''
-        self.assertTrue(False)
+        invalid_url_permutations = [reverse('accept_application', kwargs={'ethics_application_id':999}),
+                                    reverse('reject_application', kwargs={'ethics_application_id':999})]
         
+        for url in invalid_url_permutations:
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 404)
         
+    
+    
+    def shared_asserions(self, url, expected_transition):
+        '''
+            Depending on what url (accept or reject) is hit with a valid ethics_application_id 
+            then the do transition should be called with the requesting user,
+            the ethics application and the transition codename represented by expected_transition
+            
+            If this returns False then a 403 forbidden error should be raised
+            If this returns True then the request should be redirected to the index
+            page.
+        
+        '''
+        
+        with patch('review.views.do_transition') as do_transition_mock:
+            do_transition_mock.return_value = False
+            
+            response = self.client.get(url)
+            
+            do_transition_mock.assert_called_with(self.test_application, expected_transition, self.test_user)
+            self.assertEqual(response.status_code, 403)
+            
+            do_transition_mock.reset()
+            do_transition_mock.return_value = True
+            response = self.client.get(url)
+            self.assertRedirects(response, reverse('index'))
+            
+           
     def test_accept(self):
         '''
-            If the accept url is hit with a valid ethics_application_id 
-            then the do transition should be called with the requesting user,
-            the ethics application and the transition codename "accept"
-            
-            If this returns False then a 403 forbidden error should be raised
-            If this returns True then the request should be redirected to the index
-            page.
+            run the shared assertions for the accept url
         '''
         
-        self.assertTrue(False)
+        self.shared_asserions(self.valid_accept_url, 'accept')
     def test_reject(self):
         '''
-            If the reject url is hit with a valid ethics_application_id 
-            then the do transition should be called with the requesting user,
-            the ethics application and the transition codename "reject"
-            
-            If this returns False then a 403 forbidden error should be raised
-            If this returns True then the request should be redirected to the index
-            page.
+            run the shared assertions for the rejectred url
         '''
-        self.assertTrue(False)    
+        self.shared_asserions(self.valid_accept_url, 'reject')   
