bdce54d7ab3273082f29d1e6b38df3980a14db19
Jason Marshall <ctrlspc@gmail.com>
2013-02-01 11:17:17 +0000

    Added settings env variable to docs config to stop the import errors.

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,8 @@
 
 import sys, os
 
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Openethics.settings' #sets the django settings env variable to allow the autodoc imports to work.
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
